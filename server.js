7f4db9d3c12da8f8229c1262820be6b6b684da6d
Kita <ankitashah212@gmail.com>
2017-10-04 20:36:20 -0400

    server running

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(port);
