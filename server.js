1865b58d9002a30a8d57f81b546f7e1b771a2aa9
Kita <ankitashah212@gmail.com>
2017-10-05 09:52:59 -0400

    changed package.json to work with heroku

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-var router = express.Router();
 
 //make the port dynamic
 
@@ -30,69 +29,4 @@ app.listen(PORT, function () {
 
 
 
-
-
-
-// quite basic constructor
-function snack(name) {
-    if (this instanceof snack) {
-        this.name = name;
-        this.flag = false;
-    } else {
-        return new snack(name);
-    }
-
-}
-snack.prototype.eat = function () {
-
-    this.flag = true;
-}
-
-var snackArray = [];
-var cheetoes = new snack("cheetoes");
-var chips = new snack("pringles");
-snackArray.push(cheetoes);
-snackArray.push(chips);
-
-app.get("/", function (req, res) {
-    // Handlebars requires an object to be sent to the index.handlebars file.
-    console.log("get called");
-    console.log(snackArray);
-
-    // 2. Send the animals to the index.handlebars file. Remember that animals is an array and not an object.
-    res.render("index", {
-        snack: snackArray,
-    });
-});
-
-
-app.post('/snack/create', function (req, res) {
-
-    var temp = req.body;
-    console.log("post called");
-    console.log(temp);
-    var newSnack = new snack(temp.name);
-    snackArray.push(newSnack);
-    //  res.json(chars[winner]);
-    res.redirect('/');
-
-});
-
-app.post('/snack/eat', function (req, res) {
-    var temp = req.body;
-    console.log("eat called" + temp);
-
-    for (var i = 0; i < snackArray.length; i++) {
-        if (snackArray[i].name == temp.name) {
-            snackArray[i].flag = true;
-            console.log("in if condition");
-        }
-        console.log(snackArray[i]);
-        
-    }
-    //  res.json(chars[winner]);
-    res.redirect('/');
-
-});
-
-
+require('./routes/apiRoutes.js')(app);
